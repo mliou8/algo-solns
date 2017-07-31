ea05c0237b6773f7301de05ec799dc2a8181e1ac
Michael <chunghanliou@gmail.com>
2017-07-30 22:32:42 -0400

    up to date

--- UnboundedKnapsack.js
+++ UnboundedKnapsack.js
@@ -1,55 +1,39 @@
-//bottom up approach
-var cakeTypes = [
-  {weight: 7, value: 160},
-  {weight: 3, value: 90},
-  {weight: 2, value: 15},
-];
-
-var capacity = 20;
-
-maxDuffelBagValue(cakeTypes, capacity);
-
 function maxDuffelBagValue(cakeTypes, weightCapacity) {
-  // we make an array to hold the maximum possible value at every
-  //duffel bag weight capacity from 0 to weightCapacity
-  // starting each index with value 0
 
-  var maxValuesAtCapacities  [];
-  for (var i  0; i < weightCapacity; i++) {
-    maxValuesAtCapacities[i] = 0;
-  }
+   // we make an array to hold the maximum possible value at every
+   // duffel bag weight capacity from 0 to weightCapacity
+   // starting each index with value 0
+   var maxValuesAtCapacities = [];
+   for (var i = 0; i < weightCapacity + 1; i++) {
+       maxValuesAtCapacities[i] = 0;
+   }
 
-  for (var currentCapacity = 0; currentCapacity < weightCapacity; currentCapacity++) {
-    // set a variable to hold the max monetary value so for currentCapacity
-    var currentMaxValue = 0;
+   for (var currentCapacity = 0; currentCapacity <= weightCapacity; currentCapacity++) {
 
-    // we use a for loop here instead of forEach because we return infinity
-    // if we get a cakeType that weighs nothing and has a value but forEach
-    // loops always return undefined and you can't break out of them without
-    // throwing an exception
-    for (var j = 0; j < cakeTypes.length; j++) {
-      var cakeType = cakeTypes[j];
+       // set a variable to hold the max monetary value so far for currentCapacity
+       var currentMaxValue = 0;
 
-      // if a cake weighs 0 and has a positive value the value of our duffel bag is infinite!
-      if (cakeType.weight === 0 && cakeType !== 0) {
-        return Infinity;
-      }
-      // if the current cake weighs as much or less than the current weight capacity
-      // it's possible taking the cake would give a better value
-      if (cakeType.weight <= currentCapacity) {
-        // so we should check do we take the cake or not?
-        // if we use the cake, the most kilograms we can include in addition to the cake
-        // we're adding is the currenct capacity minus the cake's weight. we find the maxValuesAtCapacities
-        // value at that integer capacity in our array maxValuesAtCapacities
-        var maxValueUsingCake = cakeType.value + maxValuesAtCapacities
+       cakeTypes.forEach(function(cakeType) {
 
-      }
+           // if the current cake weighs as much or less than the current weight capacity
+           // it's possible taking the cake would give get a better value
+           if (cakeType.weight <= currentCapacity) {
 
-    }
+               // so we check: should we use the cake or not?
+               // if we use the cake, the most kilograms we can include in addition to the cake
+               // we're adding is the current capacity minus the cake's weight. we find the max
+               // value at that integer capacity in our array maxValuesAtCapacities
+               var maxValueUsingCake = cakeType.value + maxValuesAtCapacities[currentCapacity - cakeType.weight];
 
+               // now we see if it's worth taking the cake. how does the
+               // value with the cake compare to the currentMaxValue?
+               currentMaxValue = Math.max(maxValueUsingCake, currentMaxValue);
+           }
+       });
 
-  }
-
-
+       // add each capacity's max value to our array so we can use them
+       // when calculating all the remaining capacities
+       maxValuesAtCapacities[currentCapacity] = currentMaxValue;
+   }
 
 }
