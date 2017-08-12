c8e18ab3a5068f6ae309bdf978a5d4d250c4843f
mliou8 <chunghanliou@gmail.com>
2017-08-12 16:34:48 -0400

    added comments to reverselist

--- ReverseList.js
+++ ReverseList.js
@@ -2,13 +2,19 @@
 
 function reverseList(headNode) {
   var curr = headNode;
+  //initialize next and previous variables to null;
   var nextNode = null;
   var prevNode = null;
   
   while(curr) {
+    //nextnode is the next node
     nextNode = curr.next;
+    //set the current node's next to previous (since you have stored the next node)
     curr.next = prevNode;
+    // move the "previous" pointer to the current pointer, since you have to move forward in the list
     previous = current; 
+    //Set the current pointer to the next one and do this after you've already set previous, since it will change
+    //current
     current = nextNode;
   }
   return previous;
