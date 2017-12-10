88a1e71a58d9aa4d1e3cc8c443661ed954563520
mliou8 <chunghanliou@gmail.com>
2017-12-10 00:27:13 -0500

    adding single permutations

---
+++ Permutations.js
@@ -0,0 +1,26 @@
+//example input: "hey"
+// example output:
+// hye
+// hey
+// hey
+function getAllPermutations(string) {
+  // Implement results array 
+  var results = [];
+
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    // combine 
+    var firstChar = string[i];
+    var charsLeft = string.substring(0, i) + string.substring(i + 1);
+    var innerPermutations = getAllPermutations(charsLeft);
+    for (var j = 0; j < innerPermutations.length; j++) {
+      results.push(firstChar + innerPermutations[j]);
+    }
+  }
+  return results;
+}
+
